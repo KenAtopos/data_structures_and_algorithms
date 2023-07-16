fe2c2d52f87863961334ecee628a90830bd28c8e
Ken Atopos <z1158229761@outlook.com>
2023-07-17 07:10:08 +0800

    tackle leetcode problems again

--- leetcode/script.js
+++ leetcode/script.js
@@ -1,37 +1,19 @@
-// function TreeNode(val, left, right) {
-//   this.val = val === undefined ? 0 : val;
-//   this.left = left === undefined ? null : left;
-//   this.right = right === undefined ? null : right;
-// }
-
-// const bstFromPreorder = function (preorder) {
-//   if (!preorder.length) return null;
-
-//   let root = new TreeNode(preorder[0]);
-
-//   let stack = [];
-//   stack.push(root);
-
-//   let i = 1;
+// const smallestNumber = function (pattern) {
+//   let op = "",
+//     stack = [];
 
 //   debugger;
-//   while (i < preorder.length) {
-//     let topEle = stack.at(-1);
+//   for (let i = 0; i <= pattern.length; i++) {
+//     stack.push(i + 1);
 
-//     while (stack.length && preorder[i] > stack.at(-1).val) {
-//       topEle = stack.pop();
+//     if (i === pattern.length || pattern[i] === "I") {
+//       while (stack.length > 0) {
+//         op += stack.pop();
+//       }
 //     }
-
-//     let node = new TreeNode(preorder[i]);
-
-//     if (node.val < topEle.val) topEle.left = node;
-//     else topEle.right = node;
-
-//     stack.push(node);
-//     i++;
 //   }
 
-//   return root;
+//   return op;
 // };
 
-// bstFromPreorder([8, 5, 1, 7, 10, 12]);
+// smallestNumber("IIIDIDDD");
